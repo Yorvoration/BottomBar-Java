f180694341f27c7e2121223fd015094bcf3507c2
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-19 14:31:07 +0500

    bottom appbar Java

---
+++ app/src/main/java/app/app/bottombarjava/MainActivity.java
@@ -0,0 +1,23 @@
+package app.app.bottombarjava;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Sample.class));
+            }
+        }, 1500);
+    }
+}
